2730ba6aef047f1007f6aecd710eb3a362f59704
RafayShams <rafayshams@gmail.com>
2023-02-26 08:34:47 -0500

    Fixed event Listener clicking

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 //targe the hamburger icon
 const hamburger = document.querySelector('.hamburger');
-//add even listener to the hamburger icon
+//add event listener to the hamburger icon
 hamburger.addEventListener('click', function(){
   //target the mainNav ul
   const navList = document.querySelector('.mainNav ul');
- if (navList.style.visibility === "hidden" || navList.style.visibility === "") {
+  const style = getComputedStyle(navList);
+ if (style.visibility === "hidden") {
   navList.style.visibility = "visible"
  } else {
   navList.style.visibility = "hidden"
  }
 });
-//toggle the display of the mainNav from block to none when hamburger icon is clicked
+//toggle the visibility of the mainNav from visible to hidden when hamburger icon is clicked
