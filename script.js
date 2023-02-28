2f716fdcb29e5e776b0d10e9271f82891cf076bc
RafayShams <rafayshams@gmail.com>
2023-02-28 17:32:39 -0500

    added comments

--- script.js
+++ script.js
@@ -4,11 +4,12 @@ const hamburger = document.querySelector('.hamburger');
 hamburger.addEventListener('click', function(){
   //target the mainNav ul
   const navList = document.querySelector('.mainNav ul');
-  const style = getComputedStyle(navList);
+
+//toggle the visibility of the mainNav from visible to hidden when hamburger icon is clicked
+const style = getComputedStyle(navList);
  if (style.visibility === "hidden") {
   navList.style.visibility = "visible"
  } else {
   navList.style.visibility = "hidden"
  }
 });
-//toggle the visibility of the mainNav from visible to hidden when hamburger icon is clicked
